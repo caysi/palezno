14e675062736d9cc535192fbd238f90621a7c27d
Caysi <caysi@ua.fm>
2013-05-07 16:18:31 +0300

    F.php add comments

--- php/F.php
+++ php/F.php
@@ -1,9 +1,15 @@
 <?php
+/**
+* Класс для автоматического подключения файлов с функциями
+* 
+* \F::[functionName]($arg[, $arg2....]);
+*/
 class F {
 	const rootPath = __DIR__;
+	const functionsPath = __DIR__.'/functions';
 
 	static function __callStatic($name, $arguments) {
-		require_once(self::rootPath.'/functions/'.$name.'.php');
+		require_once(self::functionsPath.'/'.$name.'.php');
 		$name = '\\functions\\'.$name;
 		return $name($arguments);
 	}
