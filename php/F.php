d71842df6aed24177826439369e263cd6d11499e
Caysi <caysi@ua.fm>
2013-05-10 09:32:26 +0300

    add costant and ico

--- php/F.php
+++ php/F.php
@@ -1,15 +1,14 @@
 <?php
+define('ROOT_PATH', __DIR__);
+define('FUNCTIONS_PATH', __DIR__.'/functions');
 /**
 * Класс для автоматического подключения файлов с функциями
-* 
+*
 * \F::[functionName]($arg[, $arg2....]);
 */
 class F {
-	const rootPath = __DIR__;
-	const functionsPath = __DIR__.'/functions';
-
 	static function __callStatic($name, $arguments) {
-		require_once(self::functionsPath.'/'.$name.'.php');
+		require_once(FUNCTIONS_PATH.'/'.$name.'.php');
 		$name = '\\functions\\'.$name;
 		return $name($arguments);
 	}
