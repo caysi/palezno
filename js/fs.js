7571a27771a5b88736d8a604130cd7f43d99d627
Caysi <caysi@gg.loc>
2013-06-03 11:57:51 +0300

    some changes

--- js/fs.js
+++ js/fs.js
@@ -4,7 +4,7 @@ function getFolderContent(folder) {
 	var url = '/item/i'+item_id+'?ajax&id='+item_id+'&download=1&view=1&blocked=0&folder='+folder;
 	var response = sendAjax(url);
 	var folders = '';
-	//alert(response);
+	/*alert(response);*/
 	var re  = /parent_id: \'?(\w*)\'?[\s\S]*?\>(.*)\<\//g;
 	var re2 = /a href=\"(.[^ ]*play\&file[^ ]*)\"[\s\S]*?\>(.*)\<\/[\s\S]*?\>(.* .*)\<\/[\s\S]*?/g;
 	var reD = /\<.*href=\"([^\"]*)\".*b-file-new__link-material-download/g;
@@ -16,9 +16,9 @@ function getFolderContent(folder) {
 			if(folder == 0) {
 				folders+= '<hr>';
 			}
-			folders+= '<br>-<b>'+found[2]+'</b>';//+found[2];
+			folders+= '<br>-<b>'+found[2]+'</b>';/*+found[2];*/
 			folders+= getFolderContent(found[1]);
-			//break; //TODO DELETE
+			/*break; //TODO DELETE*/
 		}
 	}
 
@@ -27,14 +27,14 @@ function getFolderContent(folder) {
 		while ( (found = re2.exec(response)) != null) {
 			var folders = '';
 			download = reD.exec(response)[1];
-			folders+= '<br>--<a href='+found[1]+'>V</a>'; // ссылка на страницу
-			folders+= ' <a href='+download+'>D</a>'; // Download
-			folders+= ' <b>'+found[2]+'</b>'; // название
-			folders+= ' '+found[3]; // объем
-			//folders+= '<br>--'+getFileContent(found[1]); // FLV
+			folders+= '<br>--<a onclick="alert(getFileContent(\''+found[1]+'\'));return false;">URL</a>'; // FLV
+			folders+= ' <a href='+found[1]+'>V</a>'; /* ссылка на страницу */
+			folders+= ' <a href='+download+'>D</a>'; /* Download */
+			folders+= ' <b>'+found[2]+'</b>'; /* название */
+			folders+= ' '+found[3]; /* объем */
 		}
 		/*if(found) {
-		}/**/
+		}*/
 	}
 	return folders;
 }
@@ -47,7 +47,7 @@ function getFileContent(url) {
 }
 
 var HTML = getFolderContent('0');
-//var body = document.getElementsByTagName('BODY')[0];
+/*var body = document.getElementsByTagName('BODY')[0];*/
 var body = document.getElementsByClassName('l-header')[0];
 body.innerHTML = HTML;
 body.style.backgroundColor = "white";
--- php/F.php
+++ php/F.php
@@ -1,6 +1,9 @@
 <?php
 define('PALEZNO_PATH', __DIR__);
 define('FUNCTIONS_PATH', PALEZNO_PATH.'/functions');
+// определяем скрит запущет из терминала или нет
+if(PHP_SAPI == 'cli'){ define('THIS_TERMINAL', TRUE); } else { define('THIS_TERMINAL', FALSE); }
+
 /**
 * Класс для автоматического подключения файлов с функциями
 *
