7f1e5136aafdc48a590ff257d8648cb4de74c1f2
Caysi <caysi@gg.loc>
2013-07-21 22:50:52 +0300

    php add \n

--- php/functions/var_dump_old.php
+++ php/functions/var_dump_old.php
@@ -4,7 +4,7 @@ namespace functions;
 function var_dump_old() {
 	$name = '\var_dump';
 	$args = func_get_args();
-	echo "\n".'<hr><xmp>';
+	echo "\n".'<hr><xmp>'."\n";
 	eval(\F::evalStr($name, $args));
 	echo '</xmp><hr style="color: red;">'."\n";
 }
