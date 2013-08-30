9218b5f04a8867911d18836082a1816fb3a72ae6
Caysi <caysi@gg.loc>
2013-08-30 16:57:43 +0300

    php change collor in errorHandler

--- php/functions/errorHandler.php
+++ php/functions/errorHandler.php
@@ -5,8 +5,10 @@ ini_set('display_startup_errors', 0);
 
 $GLOBALS['errorNames'] = require(FUNCTIONS_PATH.'/errorNames.php');
 
-function caysi_renderErrorMessage(&$error) {
-	return '<b>'.$GLOBALS['errorNames'][$error['type']].':</b> '.$error['message'].' <b>'.$error['file'].'</b> <b>'.$error['line'].'</b><br />'."\n";
+function caysi_renderErrorMessage(&$error, $fatal=false) {
+	$class = 'error_handler';
+	if($fatal) $class.= '_fatal';
+	return '<b class="'.$class.'">'.$GLOBALS['errorNames'][$error['type']].':</b> '.$error['message'].' <b>'.$error['file'].'</b> <b>'.$error['line'].'</b><br />'."\n";
 }
 
 /*
@@ -51,8 +53,9 @@ function caysi_register_shutdown_function() {
 		|| $errno === E_COMPILE_WARNING
 		|| $errno === E_STRICT
 	) {
-		$GLOBALS['debug_info'].= caysi_renderErrorMessage($lastError);
+		$GLOBALS['debug_info'].= caysi_renderErrorMessage($lastError, true);
 		$GLOBALS['debug_info'].= F::_call('renderBacktrace', array($GLOBALS['backtrace'], 'caysi_register_tick_function'));
+		$buttonClass = 'fatal';
 	}
 
 
@@ -61,9 +64,10 @@ function caysi_register_shutdown_function() {
 		echo '<div id="shutdown_button" class="empty"></div>';
 	}
 	else {
+		if(empty($buttonClass)) $buttonClass = 'notice';
 		echo F::_call('getJS', array('shutdown'));
 		echo '<div id="shutdown_content" style="display:none;">'.$GLOBALS['debug_info'].'</div>';
-		echo '<div id="shutdown_button" class="not_empty" onclick="showContent()"></div>';
+		echo '<div id="shutdown_button" class="'.$buttonClass.'" onclick="showContent()"></div>';
 	}
 }
 //function caysi
