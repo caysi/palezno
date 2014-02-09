bcc352035f8b7b212ac3c05b58d0908bbe7c570a
Ivan Baholdin <i.baholdin@owox.com>
2014-02-09 18:52:20 +0200

    [php] add echo file

---
+++ php/echo.php
@@ -0,0 +1,89 @@
+<?php require_once('/var/www/palezno/php/F.php'); //TODO DELETE
+\F::var_dump();	exit; //TODO: DELETE
+echo '<br>'.__LINE__.'<hr>';
+
+
+$Mfc = '['.date('d-H:i:s').'] '.__FILE__.' +'.__LINE__.' '.__METHOD__.'()'."\n"; //TODO DELETE
+$Mbt = debug_backtrace();foreach($Mbt as $k=>&$Mb) {$Mfc.= '#'.$k;$Mfc.= ' '.$Mb['file'];$Mfc.= ' +'.$Mb['line'];$Mfc.= ' '.$Mb['function'].'()'."\n";} $Mfc.= "\n"; //TODO DELETE
+file_put_contents('/var/www/rozetka/logs/my.txt', $Mfc, FILE_APPEND); //TODO DELETE/**/
+
+F::vd(__FILE__,__LINE__,'', $var);
+
+	echo '<xmp>';	print_r(get_defined_vars());	echo '</xmp>';	exit;
+
+    echo '<hr><xmp>';	var_dump();	echo '</xmp><hr>';	exit; //TODO: DELETE
+    foreach($params as $p){$begin=strpos($query,'?');$c=substr($query,0,$begin).'<span style="color:red;">'.$p.'</span>';$query=$c.substr($query, $begin+1);}echo '<pre>'.$query.'</pre><hr>';return;//TODO DELETE
+
+
+	{php}	$var = $this->get_template_vars(''); var_dump($var);	exit;	{/php}{**}
+
+require_once('/var/www/if/my/debug/functions.php');
+require(ROOT_PATH.'begin.php');
+    pd(debug_backtrace());
+require(ROOT_PATH.'end.php');/**/
+
+echo 'display_errors = '.ini_get('display_errors')."<br>\n";
+echo 'display_startup_errors = '.ini_get('display_startup_errors')."<br>\n";
+echo 'error_reporting = '.ini_get('error_reporting')."<br>\n"; exit;
+
+/**  VIM
+:set filetype=diff|colorscheme dante
+%s/\n[ \t]*/ /g
+%s/&amp;/\&/g|%s/&amp;/\&/g|%s/&quot;/\"/g|%s/&lt;/</g|%s/$gt;/>/g
+%s/&amp;/\&/eg|%s/&amp;/\&/eg|%s/&quot;/\"/eg|%s/&lt;/</eg|%s/&gt;/>/eg|%s/\\n/^M/eg
+*/
+
+/*  flirt.rel-work.to.redut.net/test/apollonin/clear_cache.php
+*/    
+/*  mysql -u multi_c -pEvyuAjDod3 multi_c
+	SELECT screenname,password,lastvisit FROM profiles WHERE screenname="lex7330683" LIMIT 5;
+    SELECT id FROM staff WHERE email='baholdin@cupid.com';
+    INSERT INTO staff_account_sites (staff_id, siteID, updated) VALUES (1620, 8, NOW());
+    
+    SELECT id, ruleID FROM mail_mails WHERE content LIKE '%gg%';
+    https://www.benaughty.com/base/index.crm.php?section=linkedmails&task=editmailchapter&delivery=email&ruleID={$ruleID}&chapterID={$id}
+    ^\| {1,4}([0-9]+) +\| {1,4}([0-9]+) +\|
+    <a href="https://www.benaughty.com/base/index.crm.php?section=linkedmails&task=editmailchapter&delivery=email&ruleID=\2&chapterID=\1" >\1 \2</a>
+    
+    SELECT id,title,site_id FROM messages_html WHERE MD5(message) = '2f941e573ae90a20f1c2415b8815c600';
+        �� �����  $cont['HTML'] = preg_replace('/(href=[\"\'])[^\"\']*([\"\'])/', '\1javascript:void(null);\2', $cont['HTML']);
+        SELECT id,ruleID,siteID FROM mail_mails WHERE MD5(content) = 'b238b2374590d228e1317b49fb5dc22a'; 
+*/
+/*  git checkout -b dev-20120919-baholdin-product-bn_esp_bugfix origin/live
+    git diff origin/live..[.]dev-20120712-baholdin-product-la_fixes[ > ../diff/[...].diff]
+*/
+/*  find . | egrep "po$" | xargs grep 'To make it simplier for you' | awk '{ print $1 }' | sort -u
+    find . -name "*.po" -type f | xargs grep -n -A 1 -B 1 --color -E 'kudo'
+    find /var/www/vhosts/baholdin.dev.easydate.biz/htdocs/web/ -name "*.po" | xargs grep -n -A 1 -B 1 --color -E 'Spiele'
+    find ./locale/ -regex ".*\(DEF\|BN\)/DEF/.+\.po" | xargs grep -n --color 'asdfghjk'
+*/
+/*  ?disable_merging=1
+    http://cpd.rel.redut.net/test/yaroshenko/clear_smarty.php?site=cpd
+*/
+/*
+"C:\Program Files (x86)\Notepad++\notepad++.exe" "Z:\htdocs\web\locale\de_DE\LC_MESSAGES\site_messages\DEF\DEF\funnel3.po" "Z:\htdocs\web\locale\en_AU\LC_MESSAGES\site_messages\DEF\DEF\funnel3.po" "Z:\htdocs\web\locale\en_GB\LC_MESSAGES\site_messages\DEF\DEF\funnel3.po" "Z:\htdocs\web\locale\en_US\LC_MESSAGES\site_messages\DEF\DEF\funnel3.po" "Z:\htdocs\web\locale\es_ES\LC_MESSAGES\site_messages\DEF\DEF\funnel3.po" "Z:\htdocs\web\locale\es_MX\LC_MESSAGES\site_messages\DEF\DEF\funnel3.po" "Z:\htdocs\web\locale\fr_FR\LC_MESSAGES\site_messages\DEF\DEF\funnel3.po" "Z:\htdocs\web\locale\it_IT\LC_MESSAGES\site_messages\DEF\DEF\funnel3.po" "Z:\htdocs\web\locale\nl_BE\LC_MESSAGES\site_messages\DEF\DEF\funnel3.po" "Z:\htdocs\web\locale\nl_NL\LC_MESSAGES\site_messages\DEF\DEF\funnel3.po" "Z:\htdocs\web\locale\pt_BR\LC_MESSAGES\site_messages\DEF\DEF\funnel3.po" "Z:\htdocs\web\locale\ru_RU\LC_MESSAGES\site_messages\DEF\DEF\funnel3.po" "Z:\htdocs\web\locale\sv_SE\LC_MESSAGES\site_messages\DEF\DEF\funnel3.po" "Z:\htdocs\web\locale\tr_TR\LC_MESSAGES\site_messages\DEF\DEF\funnel3.po"
+*/
+/*  Z:\htdocs\web\inc\models\TrackUsingResource.php
+*/
+
+
+Click:
+javascript:(function(){
+    if(typeof%20VisualEvent!='undefined'){
+        if(document.getElementById('Event_display')){
+            VisualEvent.fnClose();
+        }
+        else{
+            VisualEvent.fnInit();
+        }
+    }
+    else{
+        var%20n=document.createElement('script');
+        n.setAttribute('language','JavaScript');
+        n.setAttribute('src','http://www.sprymedia.co.uk/design/event/media/js/event-loader.js');
+        document.body.appendChild(n);
+    }
+}
+)();;
+javascript:(function(){	if(typeof%20VisualEvent!='undefined'){		if(document.getElementById('Event_display')){			VisualEvent.fnClose();		}		else{			VisualEvent.fnInit();		}	}    else{		var%20n=document.createElement('script');        n.setAttribute('language','JavaScript');        n.setAttribute('src','http://www.sprymedia.co.uk/design/event/media/js/event-loader.js');        document.body.appendChild(n);    }})();;
+?>
