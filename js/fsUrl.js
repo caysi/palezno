a0d8c8e06dca8aa3ec3e210f580b248155b2f751
Caysi <caysi@gg.loc>
2014-07-19 20:29:51 +0300

    [js] fsUrl change

--- js/fsUrl.js
+++ js/fsUrl.js
@@ -2,7 +2,7 @@ var body = document.getElementsByTagName('BODY')[0];
 /*var body = document.getElementsByClassName('l-header__bg-c')[0];/**/
 
 var hostName = location.protocol+'//'+location.host;
-var playList = FS_FLOWPLAYER_CONFIG.playlist;
+var playList = FS_APLAYER_CONFIG.playlist;
 var content = '';
 for(var i in playList) {
     content+= '<h1>'+i+') '+hostName+playList[i].url+'</h1>';
