cd1243c16c43acbacf18668efb732d254313fed5
Caysi <caysi@gg.loc>
2015-01-24 20:28:21 +0200

    [js] fsUrl as m3u file fix

--- js/fsUrl.js
+++ js/fsUrl.js
@@ -1,17 +1,15 @@
-var body = document.body;
-
 var hostName = location.protocol+'//'+location.host;
 var playList = FS_APLAYER_CONFIG.playlist;
 
-var content = '<xmp>#EXTM3U'+"\n";
-content+= '#PLAYLIST:'+document.title+"\n";
+var content = '<table>'
+content+= '<tr><td>'+'#EXTM3U'+'</td></tr>';
+content+= '<tr><td>'+'#PLAYLIST:'+document.title+'</td></tr>';
 
 for(var i in playList) {
-	content+= '#EXTINF:'+playList[i].duration+','+playList[i].fsData.file_name+"\n";
-	content+= hostName+playList[i].url+"\n";
+	content+= '<tr><td>'+'#EXTINF:'+playList[i].duration+','+playList[i].fsData.file_name+'</td></tr>';
+	content+= '<tr><td>'+hostName+playList[i].url+'</td></tr>';
 }
-content+= '</xmp>';
+content+= '</table>';
 
-body.innerHTML = content;
-
+document.body.innerHTML = content;
 document.head.innerHTML = '';
