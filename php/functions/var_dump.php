4ac675250456f2f2902ee0107b0fa8bb052403fd
Caysi <caysi@gg.loc>
2015-05-12 22:50:05 +0300

    [php] fix error in var_dump

--- php/functions/var_dump.php
+++ php/functions/var_dump.php
@@ -3,7 +3,7 @@ function caysi_var_dump() {
 	$var_dump_content = '';
 	$args = func_get_args();
 
-	if(ENVIRONMENT === cli) {
+	if(ENVIRONMENT === 'cli') {
 		foreach($args as &$var) {
 			$var_dump_content.= "\n\033[37m" . '---------------------------' . "\033[0m\n";
 			$var_dump_content.= F::_call('color_cli_var_dump', array($var));
