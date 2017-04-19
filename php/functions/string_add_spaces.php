cf698eb5d054e40e5818f3bcb1de12855078ba15
Caysi <caysi@gg.loc>
2017-04-19 23:35:44 +0300

    [php] fix spaces in string_add_spaces

--- php/functions/string_add_spaces.php
+++ php/functions/string_add_spaces.php
@@ -1,6 +1,9 @@
 <?php
 function caysi_string_add_spaces($string, $needLength) {
 	$needSpaces = $needLength - strlen($string);
+	if(gettype($string) === 'integer') {
+		$needSpaces+= 2;
+	}
 	$spaces = '';
 	for($i = 0; $i < $needSpaces; $i++) {
 		$spaces.= ' ';
