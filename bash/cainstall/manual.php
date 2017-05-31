e0cee8016569a576a7379b3662845852cbc01971
Caysi <caysi@gg.loc>
2017-05-31 23:14:46 +0300

    [bash] add touchpad settings

--- bash/cainstall/manual.php
+++ bash/cainstall/manual.php
@@ -9,11 +9,19 @@ $testError = '';
 
 
 
+# http://www.linuxrussia.com/touchpad-settings-ubuntu.html
+# https://wiki.archlinux.org/index.php/Touchpad_Synaptics_(%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9)
 cmdOutTest('cat /usr/share/X11/xorg.conf.d/*synaptics.conf | grep \'Option "PalmDetect"\'', ' "1"');
-cmdOutTest('cat /usr/share/X11/xorg.conf.d/*synaptics.conf | grep \'Option "PalmDetect"\'', ' "1"');
+cmdOutTest('cat /usr/share/X11/xorg.conf.d/*synaptics.conf | grep \'Option "PalmMinWidth"\'', ' "1"');
+cmdOutTest('cat /usr/share/X11/xorg.conf.d/*synaptics.conf | grep \'Option "PalmMinZ"\'', ' "70"');
+
+cmdOutTest('cat /usr/share/X11/xorg.conf.d/*synaptics.conf | grep \'Option "TouchpadOff"\'', ' "1"');
+
 
 
 cmdOutTest('cat ~/.bashrc | grep \'^export PATH=$PATH:/var/www/palezno/bash/\'', 'export PATH=$PATH:/var/www/palezno/bash/');
+
+cmdOutTest('cat ~/.config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml | grep \'misc-exec-shell-scripts-by-default\'', 'true');
 
 installedTest('git');
 isLinkTest($_SERVER['HOME'] . '/.gitconfig',     '/var/www/palezno/git/.gitconfig');
